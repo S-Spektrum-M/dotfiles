e18cf81dd682eef2c5dcd4fd865639ed596d517f
Siddharth Mohanty <neosiddharth@gmail.com>
2021-06-10 01:34:20 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 # APT Packages
 sudo apt update
 sudo apt upgrade
-sudo apt install zsh # ZSH
+sudo apt install zsh -y # ZSH
 sudo apt install build-essential cmake vim-nox python3-dev -y # for you complete me
 sudo apt install mono-complete golang nodejs default-jdk npm -y # for you complete me
 
-# Move config files
+# Create Backups
 mv ~/.config ~/.config.bak
 mv ~/.vimrc ~/.vimrc.bak
 mv ~/.zshrc ~/.zshrc.bak
 mv ~/.tmux.conf ~/.tmux.conf.bak
+# Move config files
 cp ./config/.config ~/.config
 cp ./config/vim/.vimrc ~/.vimrc
 cp ./config/zsh/.zshrc ~/.zshrc
 cp ./config/tmux/.tmux.conf ~/.tmux.conf
-
-
 
 # Python Libraries
 
@@ -26,8 +25,10 @@ cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
 
 # For ZSH
-chsh | which zsh # Change Shell
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh; sh install.sh # oh my zsh
 
-# TODO
 # More stuff
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# TODO
